03d2851521312753f210f19c3122ad9d9fb9b367
meetmodi247 <103116887+meetmodi247@users.noreply.github.com>
2022-10-31 23:39:57 +0530

    Add files via upload

---
+++ factorial.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int fact(int n){
+    int f=1;
+    for(int i=2; i<=n; i++){
+        f*=i;
+    }
+    return f;
+}
+
+
+int main(){
+    int n;
+    cin>>n;
+
+    int sum= fact(n);
+    cout<<sum<<endl;
+}
