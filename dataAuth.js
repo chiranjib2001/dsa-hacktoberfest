7cdfd2a990484861abb7388f2704a522b38784ed
darshitshah804 <69009408+darshitshah804@users.noreply.github.com>
2023-10-12 22:39:06 +0530

    Create dataAuth.js

---
+++ dataAuth.js
@@ -0,0 +1,13 @@
+import { render } from "react-dom";
+import { ShimmerCard } from "react-shimmer-effects"
+import { ShimmerSimpleGallery } from "react-shimmer-effects"
+const Shimmer = () =>
+{
+    
+    return(
+        <>
+          <ShimmerSimpleGallery  imageHeight={200} caption />
+        </>
+    )
+}
+export default Shimmer;
